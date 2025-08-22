7e2bfc3eb23dfc80b550dbb8ed64b165da4ca71f
nameisthanhle <61102287+nameisthanhle@users.noreply.github.com>
2025-08-22 18:28:43 -0500

    Update scene.mjs

--- scene.mjs
+++ scene.mjs
@@ -1,5 +1,6 @@
- import * as THREE from 'three';
-  import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
@@ -65,4 +66,3 @@ function animate() {
 }
 
 animate();
-
